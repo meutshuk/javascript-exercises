f6a5630c5aa8a82f04de32cfae88ea22a57f0bf6
Utshuk Dhamala <me.utshuk1001@outlook.com>
2021-06-07 12:12:16 +1000

    the odin project javascript exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (word, number) {
+  let newWord = "";
+  if (number < 0) {
+    return "ERROR";
+  }
 
+  for (let index = 0; index < number; index++) {
+    newWord += word;
+  }
+  return newWord;
 };
 
 module.exports = repeatString;
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function (word) {
+  let newWOrd = word.split("");
+  let reverseWord = newWOrd.reverse();
 
+  return reverseWord.join("");
 };
 
 module.exports = reverseString;
